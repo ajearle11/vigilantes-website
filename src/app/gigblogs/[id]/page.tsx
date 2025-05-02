e531f2a20e9a6f56c55c00881f8f8f4375e4a4e9
ajearle11 <58401691+ajearle11@users.noreply.github.com>
2025-05-02 20:30:32 +0100

    Added gig blogs

--- src/app/gigblogs/[id]/page.tsx
+++ src/app/gigblogs/[id]/page.tsx
@@ -26,7 +26,11 @@ export default async function BlogPost({ params }: { params: { id: string } }) {
 
   return (
     <div className="my-10 w-full max-w-200">
-      <Link className="relative top-0 left-10 text-[#888] hover:text-white" key={post.id} href={`/gigblogs`}>
+      <Link
+        className="relative top-0 left-10 text-[#888] hover:text-white"
+        key={post.id}
+        href={`/gigblogs`}
+      >
         Back
       </Link>
       <div className="text-xl text-center uppercase text-[#888] mb-1 mt-10 font-mono">
@@ -39,12 +43,14 @@ export default async function BlogPost({ params }: { params: { id: string } }) {
         <div className="text-xl italic text-center uppercase mb-1 font-mono">
           "{post.quote}"
         </div>
-        <div className="text-md italic text-center font-mono">
-          - Sound Guy
-        </div>
+        <div className="text-md italic text-center font-mono">- Sound Guy</div>
       </div>
       <div className="text-lg text-center text-[#888] mb-7 mx-10 font-mono">
-        {post.content}
+        {post.content.split("\n").map((line, index) => (
+          <div key={index}>
+            <p className="pb-7">{line}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
