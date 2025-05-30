9568954f3241c506c725e2af9381bd176ec88726
ajearle11 <58401691+ajearle11@users.noreply.github.com>
2025-05-30 16:22:55 +0100

    Simplified

--- src/app/components/DownloadOptions.tsx
+++ src/app/components/DownloadOptions.tsx
@@ -30,9 +30,9 @@ const DownloadLayout = () => {
   return (
     <>
       <div className="flex flex-col max-w-[550px] my-30 h-[100%] justify-center">
-        <div className="text-xs text-center uppercase tracking-widest text-[#888] mb-4 font-mono">
+        {/* <div className="text-xs text-center uppercase tracking-widest text-[#888] mb-4 font-mono">
           Vigilantes Archive Access
-        </div>
+        </div> */}
         <Button
           onClick={goToGoldenRecord}
           title="Golden Record"
@@ -49,7 +49,18 @@ const DownloadLayout = () => {
           title="Life In The Fast Brain Files"
           twColor="btn-primary"
         />
-        <Button onClick={() => router.push("/gigblogs")} title="Gig Blogs" twColor="btn-primary" />
+        <Button
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+            e.preventDefault();
+            handleDownload(
+              "https://dl.dropboxusercontent.com/scl/fi/otfkvex2h5lrddyg6x2ua/Mondays.zip?rlkey=a0287bhnn2idtwg4poy5irhjr&st=ezb281nc&dl=1"
+            );
+            mutate()
+          }}
+          title="Mondays Files"
+          twColor="btn-primary"
+        />
+        {/* <Button onClick={() => router.push("/gigblogs")} title="Gig Blogs" twColor="btn-primary" /> */}
         <Button onClick={rickRoll} title="More Info..." twColor="btn-primary" />
         {/* <Button onClick={rickRoll} title="Sink" twColor="accent" /> */}
         <div className=" mt-15 mb-30 mx-4">
